61f2c4a297ced006d9a12618b8ef161f19ef58d0
Langton <langtonsd@gmail.com>
2022-09-08 20:27:52 -0700

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,13 @@
  * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
+#include <stdio.h>
+/**
+* main - main block
+* Return: 0
+*/
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle");
-	return(0);
+                puts("\"Programming is like building a multilingual puzzle");
+                return (0);
 }
