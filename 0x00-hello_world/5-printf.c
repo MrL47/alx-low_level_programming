e7bb3f2dd7a495d520f4697b2f752d4efcfc4433
Langton <langtonsd@gmail.com>
2022-09-08 20:45:22 -0700

    Hellow World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - printing the string in the put function 
+ * main - printing the printf
  *
- *Description: using the main function
+ *Description: using the printf function
  *this program prints "Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line."
  *Return: 0
- int main(void)
+ */
+int main(void)
 {
- 	printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
