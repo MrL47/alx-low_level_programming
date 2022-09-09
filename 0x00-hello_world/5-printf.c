a4cd03a97d296a93658d5ad0dee5b645e3ba677a
Langton <langtonsd@gmail.com>
2022-09-08 20:39:14 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  *this program prints "Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line."
  *Return: 0
  int main(void)
- {
+{
  	printf("Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line,\n");
 	return(0);
 }
