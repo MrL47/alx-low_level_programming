2f0fb617461f2c2b1f75eb123a961b076bc2b7e8
Langton <langtonsd@gmail.com>
2022-09-09 22:35:05 -0700

    Task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,15 +9,22 @@
  */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX /2;
+		int n;
+	int l;
 
-	if (n % 10 > 5)
-	printf("last digit of %i is %i and is greater than 5\n" n, n % 10);
-	else if (n % 10 == 0)
-	printf("last digit of %i is %i and is 0\n", n, n % 10);
-	else 
-	printf("last digit of %i is %i and is less than 6 and not 0\n" n, n, 10);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	} else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	} else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - insomnia
- * Description: the alphabet game
- * retun (0)
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(void);
-
+int main(void)
 {
-	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		putchar(alphabete);
-	putchar('n');
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
 	return (0);
 }
