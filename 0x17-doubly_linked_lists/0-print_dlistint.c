76ab5070971e2590be68eb1e3d75ff26f292ccda
Langton <langtonsd@gmail.com>
2022-12-08 05:12:23 -0800

    Update Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,14 +10,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t size;
 
+	size = 0;
 	while (h)
 	{
-		nodes++;
+
 		printf("%d\n", h->n);
 		h = h->next;
+		++size;
 	}
 
-	return (nodes);
+	return (size);
 }
